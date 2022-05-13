798cd529dd4bfeac2265fa3eb217d429118ac02e
Jean Lucas de Castro <jeanlucascastro@gmail.com>
2022-05-12 22:21:24 -0300

    Start implement axios get data

--- App.js
+++ App.js
@@ -3,33 +3,23 @@ import { StyleSheet, Text, View, Image, ScrollView, } from 'react-native';
 import logo from './assets/gedalImage.png';
 import Links from './components/Links';
 import Events from './components/Events';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Headerr from './components/Headerr';
 import SubHeaderr from './components/SubHeaderr';
+import axios from 'axios';
 
 
 export default function App() {
 
-  const event = [
-    {
-      "_id": "62673304fc326f37aa00b84c",
-      "nameEvent": "Na Rua de Olho para Lua",
-      "dateEvent": "2022-05-09T00:00:00.000Z",
-      "description": "Observacao da Luan na Praca",
-      "location": "Praca Nishinomia",
-      "howToSee": "Indo na praca",
-      "__v": 0
-    },
-    {
-      "_id": "62673304fc326f37aa00b84c",
-      "nameEvent": "Na Rua de O Lua",
-      "dateEvent": "2022-05-09T00:00:00.000Z",
-      "description": "Observacao da Luan na Praca",
-      "location": "Praca Nishinomia",
-      "howToSee": "Indo na praca",
-      "__v": 0
-    }]
-  const [ events, setEvent] = useState([]);
+
+
+
+
+
+
+
+  
+
 
   return (
     <View style={styles.container}>
@@ -37,9 +27,9 @@ export default function App() {
           {/* <Headerr /> */}
           <SubHeaderr/>
       <ScrollView> 
-        <Events Props={event} />
+        <Events  />
       </ScrollView>
-        <Links />
+        <Links />  
         <StatusBar style="auto" />
     </View>
   );
